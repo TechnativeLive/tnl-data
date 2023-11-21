df2e9d91738d494ace2b38f6ac9f2d057b75dfdc
Ross Cooper <37559715+Froskk@users.noreply.github.com>
2023-11-21 15:36:13 +0000

    fix border default colors

--- components/event-list.tsx
+++ components/event-list.tsx
@@ -12,7 +12,7 @@ export function EventList<T extends { href: string; id: string | number }>({
   return (
     <Stack w="100%" maw={800} mx="lg">
       <Title order={2}>{label}</Title>
-      <div className="flex flex-col border-0 border-l-4 border-solid rounded-bl-2xl rounded-tl-sm pl-2 pb-2 border-blue-4">
+      <div className="flex flex-col border-l-4 rounded-bl-2xl rounded-tl-sm pl-2 pb-2 border-blue-4">
         {data.map((item) => (
           <Row key={item.id} {...item} />
         ))}
